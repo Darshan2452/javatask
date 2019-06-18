c5a65181d02e1a34d2dab4f98be5a5e4316e037b
Darshan2452 <adarshan503@gmail.com>
2019-06-18 14:49:20 +0530

    overloading task

--- src/Pack/task1.java
+++ src/Pack/task1.java
@@ -1,4 +1,23 @@
 package Pack;
 
 public class task1 {
+  public void display(int a,int b){
+      System.out.println(a+b);
+  }
+  public void display(int a,float b){
+      System.out.println(a+b);
+  }
+  public void display(int a,int...b){
+      for(int i=0;i<b.length;i++) {
+          System.out.print(b[i]);
+      }
+  }
+}
+ class overl extends task1{
+    public static void main(String[] args) {
+        task1 t = new task1();
+        t.display(4, 5);
+        t.display(4,5.5f);
+        t.display(5,6,5,3,4,5,6);
+    }
 }
