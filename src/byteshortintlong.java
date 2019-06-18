8067d0d911141feda5538d860c06ae2c8dd9d733
Darshan2452 <adarshan503@gmail.com>
2019-06-18 14:33:27 +0530

    task2

---
+++ src/byteshortintlong.java
@@ -0,0 +1,16 @@
+public class byteshortintlong {
+    public static void main(String[] args){
+        int a=4;
+        int b=73;
+        short c=345;
+        short d=654;
+        byte e=23;
+        byte f=34;
+        long g=777;
+        long h =777;
+        //casting
+        byte b1=(byte)(a+b+c+d+e);
+        System.out.println(b1);
+    }
+}
+
